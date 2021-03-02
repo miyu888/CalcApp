165aa3761a7e57ce6953c0597ace26c218c7cbde
miyu888 <55343240+miyu888@users.noreply.github.com>
2021-03-03 02:55:01 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/miyu/oshima/calcapp/MainActivity.kt
@@ -0,0 +1,78 @@
+package jp.techacademy.miyu.oshima.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
+import java.lang.IllegalArgumentException
+
+        //var nStr : String = ""
+        //var nlist = ArrayList<Double>()
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.text = "計算する"
+
+
+        buttonplus.text = "Plus"
+        buttonplus.setOnClickListener{
+            //val fomula = editText1 + editText2
+            //SecondCalcApp.text = editText1.text.toString()
+            val intent = Intent(this, SecondCalcApp::class.java)
+            intent.putExtra("VALUE1", 10)
+            intent.putExtra("VALUE2",20)
+            startActivity(intent)
+
+            fun validation(percentage :String) {
+                if (percentage !in 0..100) {
+                    throw IllegalArgumentException(
+            buttonplus.setOnClickListener{view ->
+                Snackbar.make(view,"数値を正確に入力してください",Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Undo"){}.show()
+            })
+        }
+
+        buttonminus.text = "Minus"
+        buttonminus.setOnClickListener{
+            val intent = Intent(this, SecondCalcApp::class.java)
+            startActivity(intent)
+
+            buttonminus.setOnClickListener{view ->
+                Snackbar.make(view,"数値を正確に入力してください",Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Undo"){}.show()
+            }
+        }
+
+        buttonmultiply.text = "multiplize"
+        buttonmultiply.setOnClickListener{
+            val intent = Intent(this, SecondCalcApp::class.java)
+            startActivity(intent)
+
+            buttonmultiply.setOnClickListener{view ->
+                Snackbar.make(view,"数値を正確に入力してください",Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Undo"){}.show()
+            }
+        }
+
+        buttondivide.text = "divide"
+        buttondivide.setOnClickListener{
+            val intent = Intent(this, SecondCalcApp::class.java)
+            startActivity(intent)
+
+            buttondivide.setOnClickListener{view ->
+                Snackbar.make(view,"数値を正確に入力してください",Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Undo"){}.show()
+            }
+        }
+
+    }
+
+
+}
